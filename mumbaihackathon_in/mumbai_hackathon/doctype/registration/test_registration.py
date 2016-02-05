79bc8bfac717a2bc514b58333c3d22026ce8991f
Neil Trini Lasrado <neil@frappe.io>
2016-02-05 10:43:05 +0530

    Re indent animate.js

---
+++ mumbaihackathon_in/mumbai_hackathon/doctype/registration/test_registration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Neil Lasrado and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+# test_records = frappe.get_test_records('Registration')
+
+class TestRegistration(unittest.TestCase):
+	pass
