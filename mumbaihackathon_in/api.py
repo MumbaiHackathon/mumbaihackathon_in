d0d62af705c7c993d6eea2c70bb7789edea35c2c
Valmik Jangla <mail@valmik.in>
2016-02-07 11:53:15 +0530

    Addded validation for email Fixed grammar and spellings Moved Hosted By to the bottom Fixed main title alignment for mobile CSS and HTML fixes and improvements

--- mumbaihackathon_in/api.py
+++ mumbaihackathon_in/api.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+import re
 from frappe.utils import validate_email_add
 
 @frappe.whitelist(allow_guest=True)
@@ -11,11 +12,14 @@ def register(fullname, email, organization):
 	validate_email_add(email.strip(), True)
 	
 	if not frappe.get_list("Registration", filters={"email":email}):
-		regi = frappe.new_doc("Registration")
-		regi.fullname= fullname
-		regi.email= email
-		regi.organization= organization
-		regi.save()
-		return ("Registration was Success")
+		if re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+			regi = frappe.new_doc("Registration")
+			regi.fullname= fullname
+			regi.email= email
+			regi.organization= organization
+			regi.save()
+			return ("Registration was successful")
+		else:
+			return ("Please enter a valid email")
 	else:
 		return ("You have already registered")
