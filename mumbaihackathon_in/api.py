4d38ffc90b4c416408c0c2b5a607ecd42fb8ed53
Valmik Jangla <mail@valmik.in>
2016-02-08 12:42:57 +0530

    Small fixes

--- mumbaihackathon_in/api.py
+++ mumbaihackathon_in/api.py
@@ -5,18 +5,16 @@
 from __future__ import unicode_literals
 import frappe
 import re
-from frappe.utils import validate_email_add
+
 
 @frappe.whitelist(allow_guest=True)
 def register(fullname, email, organization):
-	validate_email_add(email.strip(), True)
-	
-	if not frappe.get_list("Registration", filters={"email":email}):
+	if not frappe.get_list("Registration", filters={"email": email}):
 		if re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
 			regi = frappe.new_doc("Registration")
-			regi.fullname= fullname
-			regi.email= email
-			regi.organization= organization
+			regi.fullname = fullname
+			regi.email = email
+			regi.organization = organization
 			regi.save()
 			return ("Registration was successful")
 		else:
