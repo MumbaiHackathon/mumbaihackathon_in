87da7b6bf9940a28d4e4696da38ce442f5fcf660
Raghavendra Kamath <raghu@raghukamath.com>
2018-03-21 15:36:50 +0530

    add css

--- mumbaihackathon_in/www/2018/winners.py
+++ mumbaihackathon_in/www/2018/winners.py
@@ -3,7 +3,7 @@ def get_context(context):
 		"project_name": "Briefly",
 		"project_descp": "Source based news network",
 		"github_url":"https://github.com/MumbaiHackathon/Briefly",
-		"image_url":"/assets/mumbaihackathon_in/imagesi/2018/Breifly.png",
+		"image_url":"/assets/mumbaihackathon_in/images/2018/Briefly.png",
 		"team_members": [
 			"Chirag Shetty",
 			"Neel Shah",
@@ -13,10 +13,10 @@ def get_context(context):
 	}]
 
 	first_runnerups = [{
-		"project_name": "Froogle Doodle",
+		"project_name": "Frugal Doodle",
 		"project_descp": "Doodle anywhere, anytime with anything!",
 		"github_url":"https://github.com/MumbaiHackathon/frugal-doodle",
-		"image_url":"/assets/mumbaihackathon_in/images/2018/froogle-doodle.png",
+		"image_url":"/assets/mumbaihackathon_in/images/2018/frugal-doodle.png",
 		"team_members": [
 			"Arpita Karkera",
 			"Priya Nayak",
@@ -29,7 +29,7 @@ def get_context(context):
 	second_runnerups = [{
 		"project_name": "Crypto Doc",
 		"project_descp": "Storing personal identification documents on blockchain",
-		"image_url": "/assets/mumbaihackathon_in/images/2018/default.png",
+		"image_url": "/assets/mumbaihackathon_in/images/2018/crypto-doc.png",
 		"github_url":"https://github.com/MumbaiHackathon/CryptoDoc",
 		"team_members": [
 			"Saurav Kanchan",
@@ -39,7 +39,8 @@ def get_context(context):
 		]
 		}, {
 		"project_name": "Penetration Testing Mobile App",
-		"image_url": "/assets/mumbaihackathon_in/images/2018/default.png",
+		"project_descp": "Hybrid application for vulnerability assessment and penetration testing",
+		"image_url": "/assets/mumbaihackathon_in/images/2018/pen-test.png",
 		"github_url":"https://github.com/MumbaiHackathon/mumbai-hackathon",
 		"team_members": [
 			"Rahul Gupta",
@@ -47,7 +48,6 @@ def get_context(context):
 			"Vijay Jha",
 			"Tejas Barambe"
 		]
-		}
 	}]
 
 	context.winners = winners
