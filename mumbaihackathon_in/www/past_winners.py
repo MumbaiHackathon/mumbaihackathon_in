3f3acb67bb16a2b771bca3c9a22cd165890b26ad
scmmishra <scm.mymail@gmail.com>
2019-02-18 20:26:00 +0530

    feat: Added Past winners page

---
+++ mumbaihackathon_in/www/past_winners.py
@@ -0,0 +1,172 @@
+def get_context(context):
+    context.winners = [{
+        'year': '2018',
+        'winners': [{
+            "project_name": "Briefly",
+            "project_descp": "Source based news network",
+            "github_url": "https://github.com/MumbaiHackathon/Briefly",
+            "image_url": "/assets/mumbaihackathon_in/images/2018/Briefly.png",
+            "team_members": [
+                "Chirag Shetty",
+                "Neel Shah",
+                "Shivam Pawase",
+                "Nishchith Shetty"
+            ]
+        }],
+        'first_runnerups': [{
+            "project_name": "Frugal Doodle",
+            "project_descp": "Doodle anywhere, anytime with anything!",
+            "github_url": "https://github.com/MumbaiHackathon/frugal-doodle",
+            "image_url": "/assets/mumbaihackathon_in/images/2018/frugal-doodle.png",
+            "team_members": [
+                "Arpita Karkera",
+                "Priya Nayak",
+                "Sunaina Punyani",
+                "Piyusha Sawant",
+                "Deep Kawa"
+            ]
+        }],
+        'second_runnerups': [{
+            "project_name": "Crypto Doc",
+            "project_descp": "Storing personal identification documents on blockchain",
+            "image_url": "/assets/mumbaihackathon_in/images/2018/crypto-doc.png",
+            "github_url": "https://github.com/MumbaiHackathon/CryptoDoc",
+            "team_members": [
+                "Saurav Kanchan",
+                "Yash Puthran",
+                "Jatin Parab",
+                "Owais Khan"
+            ]
+        }, {
+            "project_name": "Penetration Testing Mobile App",
+            "project_descp": "Hybrid application for vulnerability assessment and penetration testing",
+            "image_url": "/assets/mumbaihackathon_in/images/2018/pen-test.png",
+            "github_url": "https://github.com/MumbaiHackathon/mumbai-hackathon",
+            "team_members": [
+                "Rahul Gupta",
+                "Aniket Bharti",
+                "Vijay Jha",
+                "Tejas Barambe"
+            ]
+        }]
+    }, {
+        'year': '2017',
+        'winners': [{
+            "project_name": "Spockpy",
+            "github_url":"https://github.com/MumbaiHackathon/spockpy",
+            "image_url":"/assets/mumbaihackathon_in/images/spockpy.png",
+            "team_members": [
+                "Achilles Rasquinha",
+                "Ameya Shenoy"
+            ]
+        },{
+            "project_name": "Clipper Co.",
+            "github_url":"https://github.com/MumbaiHackathon/Clipper-Co.",
+            "image_url":"/assets/mumbaihackathon_in/images/clipper_co.png",
+            "team_members": [
+                "Breejesh Rathod",
+                "Ankitesh Gupta",
+                "Akshit Gandhi",
+                "Dharin Parekh",
+                "Shharrnam Chhatpar"
+            ]
+        }],
+        'first_runnerups': [{
+            "project_name": "Code Collaboration",
+            "github_url":"https://github.com/MumbaiHackathon/RT-Code-Collab",
+            "image_url":"/assets/mumbaihackathon_in/images/code_collab.gif",
+            "team_members": [
+                "Aditya Kurhade",
+                "Arun Nair",
+                "Omkar Dubas",
+                "Suyog Gadhave",
+                "Rushabh Wadkar"
+            ]
+        }, {
+            "project_name": "Deep Diagnosis",
+            "github_url":"https://github.com/MumbaiHackathon/DeepDiagnosis",
+            "image_url":"/assets/mumbaihackathon_in/images/deep_diagnosis.png",
+            "team_members": [
+                "Shaikh Jawad",
+                "Khan Maaz",
+                "Sayed Afzal",
+                "Aditya Mishra",
+                "Danish Shah"
+            ]
+        }],
+
+        'second_runnerups': [
+            {
+                "project_name": "Passworld",
+                "github_url":"https://github.com/MumbaiHackathon/passworld",
+                "team_members": [
+                    "Rishabh Nambiar",
+                    "Ayush Sindhwani",
+                    "Sai Nadkarni"
+                ]
+            }, {
+                "project_name": "Personal Chef",
+                "github_url":"https://github.com/MumbaiHackathon/Personal-Chef",
+                "team_members": [
+                    "Siddhesh Rane",
+                    "Sarthak Dadhakar",
+                    "Rohan Patil"
+                ]
+            },
+            {
+                "project_name": "TrackIt",
+                "github_url":"https://github.com/MumbaiHackathon/Trackit",
+                "team_members": [
+                    "Gary Mendonca",
+                    "Glen Martin",
+                    "Royston Lobo",
+                    "Aaron Pereira",
+                    "Swapnil Naik"
+
+                ]
+            },
+            {
+                "project_name": "Simplify",
+                "github_url":"https://github.com/MumbaiHackathon/simplify",
+                "team_members": [
+                    "Sumit Busa",
+                    "Harsh Chheda",
+                    "Vivek Vora",
+                    "Parth Velani",
+                    "Akshen Kadakia"
+
+                ]
+            },
+            {
+                "project_name": "Sound Analysis",
+                "github_url":"https://github.com/MumbaiHackathon/SoundAnalysis",
+                "team_members": [
+                    "Nishchith",
+                    "Prafful",
+                    "Pravar",
+                    "Ahaan",
+                    "Divya"
+                ]
+            },
+            {
+                "project_name": "WatchDroid",
+                "github_url":"https://github.com/MumbaiHackathon/WatchDroidRestart",
+                "team_members": [
+                    "Mayur Shinde",
+                    "Aishwarya Korde",
+                    "Raj Devkar",
+                    "Tejaswini Tambe",
+                    "Bhavesh Atwal"
+                ]
+            }, {
+                "project_name": "C Library",
+                "github_url":"https://github.com/MumbaiHackathon/New-C-Library",
+                "team_members": [
+                    "Anto Christopher",
+                    "Akash Garule",
+                    "Aarish Iyer",
+                    "Haramrit Singh",
+                    "Rijan Ramavat"
+                ]
+            }]
+        }]
