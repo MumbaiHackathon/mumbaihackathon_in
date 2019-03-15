d622c1ef816b6ca72677e65ef90a87290e6835ae
scmmishra <scm.mymail@gmail.com>
2019-03-15 11:52:45 +0530

    feat: Added evaluation flow

--- mumbaihackathon_in/mumbai_hackathon/doctype/evaluation/evaluation.js
+++ mumbaihackathon_in/mumbai_hackathon/doctype/evaluation/evaluation.js
@@ -2,7 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Evaluation', {
-	// refresh: function(frm) {
+	evaluation_round: function(frm) {
+		frappe.db.get_doc("Evaluation Round", frm.doc.evaluation_round).then(data => {
+			let criteria = data.criteria.map((c) => {
+				return {criteria: c.criteria}
+			})
+			criteria.forEach((c) => {
+				frm.add_child("evaluation", c)
+			})
+			frm.refresh()
+		})
+	},
+});
 
-	// }
+
+frappe.ui.form.on("Project Evaluation", {
+	score: function(frm, cdt, cdn) {
+		console.log("Hello")
+		let row = locals[cdt][cdn];
+		console.log(row)
+	}
 });
--- mumbaihackathon_in/mumbai_hackathon/doctype/evaluation/evaluation.py
+++ mumbaihackathon_in/mumbai_hackathon/doctype/evaluation/evaluation.py
@@ -3,8 +3,16 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Evaluation(Document):
-	pass
+	def validate(self):
+		evaluations = self.get_all_children()
+		total = 0
+		for entry in evaluations:
+			if entry.score > 10:
+				frappe.throw("Please give a score between 0 to 10 only")
+			total += entry.score
+		self.total = total
+
