53bb614c59ca5f6af9011eeedb5ab9e2b0a051ac
Summayya <sumaiyahas2908@gmail.com>
2022-03-17 02:13:12 +0530

    refactor: make global settings for hackathon

---
+++ mumbaihackathon_in/mumbai_hackathon/doctype/hackathon_setting/hackathon_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Neil Lasrado and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Hackathon Setting', {
+	// refresh: function(frm) {
+
+	// }
+});
