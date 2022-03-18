2009ccc9e34cf02e6a4185b5e6eb9c8b4b9ab330
Summayya <sumaiyahas2908@gmail.com>
2022-03-18 20:49:47 +0530

    refactor: insert team details

--- mumbaihackathon_in/api.py
+++ mumbaihackathon_in/api.py
@@ -5,21 +5,30 @@
 from __future__ import unicode_literals
 import frappe
 import re
+import json
 
 
 @frappe.whitelist(allow_guest=True)
-def register(fullname, email, organization, team_name=None, source="Form", registration_method=None):
-	return "Mumbai Hackathon has been cancelled for the year 2020"
+def register(values):
+	values = json.loads(values)
 	year = frappe.utils.today()[:4]
-
 	regi = frappe.new_doc("Registration")
-	regi.fullname = fullname
-	regi.email = email
-	regi.team_name = team_name
-	regi.organization = organization
-	regi.source = source
-	regi.registration_method = registration_method
+	regi.fullname = values['fullname']
+	regi.email = values['email']
+	regi.team_name = values['team_name']
+	regi.organization = values['organization']
+	regi.source = values['source']
+	regi.registration_method = values['registration_method']
+	regi.team_type = values['team_type']
 	regi.year = year
+	regi.fullname_2 = values['fullname_2']
+	regi.email_2 = values['email_2']
+	regi.fullname_3 = values['fullname_3']
+	regi.email_3 = values['email_3']
+	regi.fullname_4 = values['fullname_4']
+	regi.email_4 = values['email_4']
+	regi.fullname_5 = values['fullname_5']
+	regi.email_5 = values['email_5']
 
 	try:
 		regi.save()
